33516a890814d337ee4be33e323cec1fefc1a113
Sergey <43184096+b-c-v@users.noreply.github.com>
2023-05-21 13:39:16 +0300

    Saved Jenkins login password in variable Anisible  and fixed CIDR in Terraform

--- 1_environment/Terraform/modules/webserver/main.tf
+++ 1_environment/Terraform/modules/webserver/main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "cicd_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -24,14 +24,14 @@ resource "aws_security_group" "cicd_sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
